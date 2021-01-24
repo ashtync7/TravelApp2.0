0591dda805ff86abc3d922ad8f37d2618aa993a0
Ashtyn Ce <ashtyncza@gmail.com>
2021-01-23 20:56:06 -0500

    css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,65 +1,81 @@
 import React, { Component, Fragment, useState, useEffect } from "react";
 import { Switch, Route, NavLink, useHistory } from "react-router-dom";
 import TheContext from './TheContext';
-import Home from "./components/home/Home";
+import Home from './components/Home';
+import Destinations from './components/Destinations';
+import CustomVacations from './components/CustomVacation';
+import TravelResources from './components/TravelResources';
+import AboutUs from './components/About';
 import NotFound from "./components/404/NotFound.js";
-import SignUp from "./components/auth/SignUp";
-import LogIn from "./components/auth/LogIn";
-import Profile from "./components/profile/Profile";
 import actions from "./api/index";
 import { NotificationContainer, NotificationManager } from 'react-notifications';
+
 const App = () => {
 
 
-
   return (
-    <TheContext.Provider value={{ history, user, setUser }}>
-
-      {user?.email}
-      <nav>
-        <NavLink to="/">Home</NavLink>
-
-        {user ? (
-          <Fragment>
-            <NavLink onClick={logOut} to="/">
-              Log Out
-            </NavLink>
-            <NavLink to="/profile">Profile</NavLink>
-          </Fragment>
-        ) : (
-            <Fragment>
-              <NavLink to="/sign-up">Sign Up</NavLink>
-              <NavLink to="/log-in">Log In</NavLink>
-            </Fragment>
-          )}
+    <div>
+      <nav className="navBar">
+        <ul>
+          <Link to='/'><img id="logo" src="images/WTAlogoVan1.png"></img></Link>
+          <Link to='/'><li>Home</li></Link>
+          <Link to={`/Destinations`}><li>Destinations</li></Link>
+          <Link to={`/CustomVacations`}><li>Custom Vacations</li></Link>
+          <Link to={`/TravelResources`}><li>Travel Resources</li></Link>
+          <Link to={`/AboutUs`}><li>About Us</li></Link>
+        </ul>
       </nav>
+      <div id="midVanDiv">
+        {/* <Link to={`/`}> */}
+        <img src="images/WTAlogoVan1.png" alt="Our Van Logo" id="middleLogo" />
+        {/* </Link> */}
+      </div>
+      <nav className="menu-wrap">
+        <input type="checkbox" class="toggler" />
+        <div class="hamburger">
+          <div></div>
+        </div>
+        <div class="menu">
+          <div>
+            <div>
+              <ul>
+                <Link to={`/`} id="menu2li"><li>Home</li></Link>
+                <Link to={`/Destinations`} id="menu2li"><li>Destinations</li></Link>
+                <Link to={`/CustomVacations`} id="menu2li"><li>Custom Vacations</li></Link>
+                <Link to={`/TravelResources`} id="menu2li"><li>Travel Resources</li></Link>
+                <Link to={`/AboutUs`} id="menu2li" ><li>About Us</li></Link>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+
+      {/* <div>
+        <ul>
+          <li><a href="#" title="home">home</a></li>
+          <li><a href="#" title="destinations">Destinations</a></li>
+          <li><a href="#" title="link1">Custom Vacations</a></li>
+          <li><a href="#" title="link1">Travel Resources</a></li>
+          <li><a href="#" title=/About Us"/About Us</a></li>
+        </ul>
+      </div> */}
+
       <Switch>
-        <Route exact path="/" render={(props) => <Home {...props} />} />
-        <Route
-          exact
-          path="/sign-up"
-          render={(props) => <SignUp {...props} setUser={setUser} />}
-        />
-        <Route
-          exact
-          path="/log-in"
-          render={(props) => <LogIn {...props} setUser={setUser} />}
-        />
-        <Route
-          exact
-          path="/profile"
-          render={(props) => <Profile {...props} />}
-        />
-
-        <Route component={NotFound} />
+        <Route exact path='/' render={() => <Home />} />
+        <Route exact path='/Destinations' render={() => <Destinations />} />
+        <Route exact path='/CustomVacations' render={() => <CustomVacations />} />
+        <Route exact path='/TravelResources' render={() => <TravelResources />} />
+        <Route exact path='/AboutUs' render={() => <AboutUs />} />
       </Switch>
-      {!user && <GoogleAuth setUser={setUser} />}
-      {!user && <GoogleAuthLogin setUser={setUser} />}
 
-      <NotificationContainer />
 
-    </TheContext.Provider>
+      {/* <footer>
+      <h1>© 2020 Wanderlust Travel Agency | Designed by Jada Woody and Ashtyn Czapansky</h1>
+      <h2>123 Fake Address Rd. | Some Small Town, State51 11111 USA | <a href="tel:+0000000000" id="phoneCall">(000)000-0000</a></h2>
+    </footer> */}
 
+    </div>
   )
 
 }
