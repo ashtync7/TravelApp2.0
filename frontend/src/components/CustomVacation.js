57879c1463dd9d04555f524d9b2e2f8bf2e72b94
Ashtyn Ce <ashtyncza@gmail.com>
2021-02-01 20:58:57 -0500

    making cart prettier

--- frontend/src/components/CustomVacation.js
+++ frontend/src/components/CustomVacation.js
@@ -15,23 +15,33 @@ function CustomVacation(props) {
 
 
     const showCart = () => {
+        // Creates an empty array named HTML in which we will be able to push HTML into
         let html = [];
+        // Creates a opy of the cart data
         let cartCopy = { ...cart }
+        // Creates a for loop which will allow us to push data to the empty array
         for (let eachcustom in cartCopy) {
             html.push(
-                <ul>
-                    <li>
-                        <h3>{cartCopy[eachcustom].name}</h3>
-                    </li>
-                    <li>
-                        <img src={cartCopy[eachcustom].image}></img>
-                    </li>
-                </ul>
+                <div className="cartDiv">
+                    <ul>
+                        <li>
+                            {/* Pushing the name and image of each added data piece/custom */}
+                            {/* Take the cartCopy, pass the prop “eachcustom” to it, which is whichever city we add-- And then return the name of the city (.name) */}
+                            <h3>{cartCopy[eachcustom].name}</h3>
+                        </li>
+                        <li>
+                            {/* same as before but we are adding the image of the "eachcustom" instead of name */}
+                            <img src={cartCopy[eachcustom].image} className="cartImage"></img>
+                        </li>
+                    </ul>
+                </div>
             );
         }
         // cart.shift()
         return (
-            <div>
+            <div className="cartReturn">
+                {/* return a div which holds our array 'HTML' in which we pushed our cart to. now HTML is the rendered version of cart */}
+                <h2 className="cartTitle1" >Your Custom Vacation</h2>
                 {html}
             </div>
         )
@@ -40,21 +50,6 @@ function CustomVacation(props) {
 
 
     }
-    // cart.map((eachcustom) => {
-    //     return (
-    //         <ul>
-    //             <li>
-    //                 Name: <h3>{eachcustom.name}</h3>
-    //             </li>
-
-    //             <li>
-
-    //                 <img src={eachcustom.image} />
-    //             </li>
-    //         </ul>
-    //     )
-    // })
-
 
 
     // new function customList = how we list out all customs from customs.json file. 
@@ -81,7 +76,8 @@ function CustomVacation(props) {
     }
     return (
         <div>
-            <button onClick={(e) => console.log({ cart })}>Cart</button>
+            {/* dont need cart button because new method "showCart" auto renders upon adding the customization! */}
+            {/* <button onClick={(e) => console.log({ cart })}>Cart</button> */}
             {/* <button onClick={(e) => showCart()}>Cart</button> */}
             {showCart()}
             {customList()}
