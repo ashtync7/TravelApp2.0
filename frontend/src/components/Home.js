0f33d4825b225114601b0d9d48400bcc3adb2614
Ashtyn Ce <ashtyncza@gmail.com>
2021-02-22 16:00:49 -0500

    netlifyyyyyyy

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -15,7 +15,7 @@ function Home(props) {
             <div id="formStuff">
                 {/* <div><p id="formIntro">Let's start planning your trip! Tell us about what kind of vacation you want to take!</p></div> */}
                 <div>
-                    <form enctype="text/plain" name="contact" action="mailto:jadawoody@gmail.com" method="POST" id="chickenwings">
+                    <form enctype="text/plain" data-netlify="true" name="contact" action="mailto:jadawoody@gmail.com" method="POST" id="chickenwings">
                         <div className="formDiv1">
                             <input type="text" id="cheeseburgers" name="name" className="input1" placeholder="First & Last Name" name="Name"></input></div>
                         <div className="formDiv2">
